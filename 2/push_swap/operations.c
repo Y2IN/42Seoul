ba522bede699a987276fab1f198e33e53be934ca
Y2IN <dpdls0228@daum.net>
2022-11-03 19:03:03 +0900

    feat : s* 구현

--- 2/push_swap/operations.c
+++ 2/push_swap/operations.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   operation1.c                                       :+:      :+:    :+:   */
+/*   operations.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yje <yje@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 16:33:31 by yje               #+#    #+#             */
-/*   Updated: 2022/11/03 16:37:52 by yje              ###   ########.fr       */
+/*   Updated: 2022/11/03 18:58:53 by yje              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,40 @@
 
 void	sa(t_var *stacks)
 {
-	t_node	*tmp1;
-	t_node	*tmp2;
+	int tmp1;
+	int tmp2;
 
 	tmp1 = pop_top(stacks->stack_a);
 	tmp2 = pop_top(stacks->stack_a);
 	push_top(stacks->stack_a, tmp1);
 	push_top(stacks->stack_a, tmp2);
 	write(1, "sa\n", 3);
+}
+
+void	sb(t_var *stacks)
+{
+	int tmp1;
+	int tmp2;
+
+	tmp1 = pop_top(stacks->stack_b);
+	tmp2 = pop_top(stacks->stack_b);
+	push_top(stacks->stack_b, tmp1);
+	push_top(stacks->stack_b, tmp2);
+	write(1, "sb\n", 3);
+}
+
+void	ss(t_var *stacks)
+{
+	int tmp1;
+	int tmp2;
+
+	tmp1 = pop_top(stacks->stack_a);
+	tmp2 = pop_top(stacks->stack_a);
+	push_top(stacks->stack_a, tmp1);
+	push_top(stacks->stack_a, tmp2);
+	tmp1 = pop_top(stacks->stack_b);
+	tmp2 = pop_top(stacks->stack_b);
+	push_top(stacks->stack_b, tmp1);
+	push_top(stacks->stack_b, tmp2);
+	write(1, "ss\n", 3);
 }
--- 2/push_swap/push_swap.h
+++ 2/push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: yje <yje@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/03 15:19:19 by yje               #+#    #+#             */
-/*   Updated: 2022/11/03 16:38:49 by yje              ###   ########.fr       */
+/*   Updated: 2022/11/03 18:50:54 by yje              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// typedef struct s_node
-// {
-// 	int				val;
-// 	struct s_node	*left;
-// 	struct s_node	*right;
-// }	t_node;
+typedef struct s_node
+{
+	int				val;
+	struct s_node	*left;
+	struct s_node	*right;
+}	t_node;
 
-// typedef struct s_stack
-// {
-// 	struct s_node	*top;
-// 	struct s_node	*bottom;
-// }	t_stack;
+typedef struct s_stack
+{
+	struct s_node	*top;
+	struct s_node	*bottom;
+}	t_stack;
 
 typedef struct s_var
 {
 	struct s_stack	*stack_a;
 	struct s_stack	*stack_b;
-	int	list;
-	int list_size;
-	int a_size;
-	int b_size;
-	
-	
 } t_var;
+
+
+/* operationp.c */
+void	pa(t_var *stacks);
+void	pb(t_var *stacks);
+
+/* operations.c */
+void	sa(t_var *stacks);
+void	sb(t_var *stacks);
+void	ss(t_var *stacks);
+
 
 #endif
