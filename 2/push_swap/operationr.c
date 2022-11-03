d870488c0b9fa02030c2b76d858ce6d1bd627576
Y2IN <dpdls0228@daum.net>
2022-11-03 19:02:55 +0900

    feat : r* 구현

---
+++ 2/push_swap/operationr.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operationr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yje <yje@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 18:45:34 by yje               #+#    #+#             */
+/*   Updated: 2022/11/03 19:01:48 by yje              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ra(t_var *stacks)
+{
+	t_node	*tmp;
+	
+	tmp = pop_top(stacks->stack_a);
+	push_bottom(stacks->stack_a, tmp);
+	write(1, "ra\n", 3);
+}
+
+void	rb(t_var *stacks)
+{
+	t_node	*tmp;
+	
+	tmp = pop_top(stacks->stack_b);
+	push_bottom(stacks->stack_b, tmp);
+	write(1, "rb\n", 3);
+}
+
+void	rr(t_var *stacks)
+{
+	t_node	*tmp;
+	
+	tmp = pop_top(stacks->stack_a);
+	push_bottom(stacks->stack_a, tmp);
+	tmp = pop_top(stacks->stack_b);
+	push_bottom(stacks->stack_b, tmp);
+	write(1, "rr\n", 3);
+}
+
+void	rra(t_var *stacks)
+{
+	t_node	*tmp;
+	
+	tmp = pop_bottom(stacks->stack_a);
+	push_top(stacks->stack_a, tmp);
+	write(1, "rra\n", 3);
+}
+
+void	rrb(t_var *stacks)
+{
+	t_node	*tmp;
+	
+	tmp = pop_bottom(stacks->stack_b);
+	push_top(stacks->stack_b, tmp);
+	write(1, "rrb\n", 3);
+
+}
+
+void	rrr(t_var *stacks)
+{
+	t_node	*tmp;
+	
+	tmp = pop_bottom(stacks->stack_a);
+	push_top(stacks->stack_a, tmp);
+	tmp = pop_bottom(stacks->stack_b);
+	push_top(stacks->stack_b, tmp);
+	write(1, "rrr\n", 3);
+}
