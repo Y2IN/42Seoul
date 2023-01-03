e899e6125f3ec381b4e5bf2a5dc3f1f5712ac848
Y2IN <dpdls0228@daum.net>
2023-01-03 16:20:41 +0900

    feat : ex01 ing

--- 4/cpp/cpp00/ex01/Contact.hpp
+++ 4/cpp/cpp00/ex01/Contact.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   contact.hpp                                        :+:      :+:    :+:   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yje <yje@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 13:10:36 by yje               #+#    #+#             */
-/*   Updated: 2022/12/30 15:11:13 by yje              ###   ########.fr       */
+/*   Updated: 2023/01/03 16:19:27 by yje              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,24 @@
 #include <iostream>
 
 class Contact {
-	private:
+private:
 	std::string firstName;
     std::string lastName;
     std::string nickName;
     std::string phoneNumber;
     std::string darkestSecret;
-}
+
+public:
+	Contact();
+	Contact(std::string first_name,
+		std::string last_name,
+		std::string nickname,
+		std::string phone_number,
+		std::string darkest_secret);
+	std::string get_first_name();
+	std::string get_last_name();
+	std::string get_nickname();
+	~Contact();
+
+};
 #endif
--- 4/cpp/cpp00/ex01/PhoneBook.cpp
+++ 4/cpp/cpp00/ex01/PhoneBook.cpp
@@ -6,7 +6,23 @@
 /*   By: yje <yje@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 13:03:53 by yje               #+#    #+#             */
-/*   Updated: 2022/12/30 15:32:36 by yje              ###   ########.fr       */
+/*   Updated: 2023/01/03 16:16:57 by yje              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Contact.hpp"
+
+Contact::Contact(
+	std::string firstName,
+	std::string lastName,
+	std::string nickName,
+	std::string phoneNumber,
+	std::string darkestSecret
+) 
+{
+	this->firstName = firstName;
+	this->lastName = lastName;
+	this->nickName = nickName;
+	this->phoneNumber = phoneNumber;
+	this->darkestSecret = darkestSecret;
+}
