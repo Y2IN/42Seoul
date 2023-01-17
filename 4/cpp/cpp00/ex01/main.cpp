c3b6ad97586c7cb5a0ce78278c510c9adf510ff2
Y2IN <dpdls0228@daum.net>
2023-01-17 12:09:54 +0900

    cpp_ex01 main

--- 4/cpp/cpp00/ex01/main.cpp
+++ 4/cpp/cpp00/ex01/main.cpp
@@ -3,17 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yje <yje@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: yje <yje@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 15:13:35 by yje               #+#    #+#             */
-/*   Updated: 2023/01/06 18:32:59 by yje              ###   ########.fr       */
+/*   Updated: 2023/01/17 12:08:42 by yje              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Contact.hpp"
+// #include "PhoneBook.hpp"
+#include <string>
+#include <iostream>
 
 int main() {
-	Contact c = Contact("1", "2", "3", "4", "5");
+	PhoneBook phonebook = PhoneBook();
+	std::string cmd;
 
-	c.getFirstName();
+	std::cout << "My Awesome PhoneBook" << std::endl;
+	std::cout << "command> ";
+	while (std::getline(std::cin, cmd))
+	{
+		if (cmd == "ADD")
+		{
+			phonebook.AddContact();
+			std::cout << "command> ";
+		}
+		else if (cmd == "SEARCH")
+		{
+			phonebook.search();
+			std::cout << "command> ";
+		}
+		else if(cmd == "EXIT")
+		{
+			std::cout << "exit" <<std::endl;
+			break;
+		}
+		else
+		{
+			std::cout << "Invalid input\n\n";
+      		std::cout << "command> ";
+		}
+	}
+	if (std::cin.eof()) {
+    std::cout << "EOF -> Program Exit" << std::endl;
+    return (0);
+	}
+	return (0);
 }
