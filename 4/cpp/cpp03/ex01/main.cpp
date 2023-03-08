fe41315c4f03c093d4671ee4cb990259ff1a26c7
Y2IN <dpdls0228@daum.net>
2023-03-08 20:40:44 +0900

    feat : ex01 구현

--- 4/cpp/cpp03/ex01/main.cpp
+++ 4/cpp/cpp03/ex01/main.cpp
@@ -6,22 +6,48 @@
 /*   By: yje <yje@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 17:44:34 by yje               #+#    #+#             */
-/*   Updated: 2023/02/23 17:51:35 by yje              ###   ########.fr       */
+/*   Updated: 2023/03/08 16:27:45 by yje              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
+
 int main()
 {
-	ScavTrap a("a");
-	ScavTrap b(a);
-	ScavTrap c;
-	c = b;
+	std::string s1 = "yein";
+	std::string s2 = "yje";
+	std::string s3 = "je";
+	ScavTrap a(s1);
+	ScavTrap b(s2);
+	ClapTrap c(s3);
 
-	a.attack("b");
-	a.beRepaired(200);
-	a.guardGate();
-	a.takeDamage(99);
-	a.attack("b");
+
+	std::cout << std::endl;
+	a.attack(s3);
+	c.takeDamage(a.getAttackDamage());
+	std::cout << std::endl;
+
+	std::cout << std::endl;
+	a.attack(s2);
+	b.takeDamage(a.getAttackDamage());
+	b.beRepaired(a.getAttackDamage());
+	b.beRepaired(a.getAttackDamage());
+	b.beRepaired(a.getAttackDamage());
+	std::cout << std::endl;
+
+	std::cout << std::endl;
+	a.attack(s3);
+	b.takeDamage(a.getAttackDamage());
+	a.attack(s3);
+	b.takeDamage(a.getAttackDamage());
+	a.attack(s3);
+	b.takeDamage(a.getAttackDamage());
+	std::cout << std::endl;
+
+	std::cout << std::endl;
+	b.guardGate();
+	std::cout << std::endl;
+	return 0;
+
 
 }
