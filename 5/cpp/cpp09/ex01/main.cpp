8168813ea754e82a775ec4d674beaa49903c4545
Y2IN <dpdls0228@daum.net>
2023-03-29 17:26:22 +0900

    01 ~ing

--- 5/cpp/cpp09/ex01/main.cpp
+++ 5/cpp/cpp09/ex01/main.cpp
@@ -1,15 +1,15 @@
-#include "RPN.hpp"
+// #include "RPN.hpp"
 
-void leaks() {
-  system("leaks RPN");
-}
+// void leaks() {
+//   system("leaks RPN");
+// }
 
-int main(int ac, char **av) {
-  atexit(leaks);
-  if (ac != 2) {
-    std::cerr <<"Error" << std::endl;
-    return 0;
-  }
-  RPN rpn(av[1]);
-  return 0;
-}
+// int main(int ac, char **av) {
+//   atexit(leaks);
+//   if (ac != 2) {
+//     std::cerr <<"Error" << std::endl;
+//     return 0;
+//   }
+//   RPN rpn(av[1]);
+//   return 0;
+// }
