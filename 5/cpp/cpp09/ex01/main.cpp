4270a3d048fc5e5de11d6a3863b1ece305d94d08
Y2IN <dpdls0228@daum.net>
2023-03-31 01:27:47 +0900

    cpp09_01

--- 5/cpp/cpp09/ex01/main.cpp
+++ 5/cpp/cpp09/ex01/main.cpp
@@ -1,15 +1,16 @@
-// #include "RPN.hpp"
+#include "RPN.hpp"
 
 // void leaks() {
 //   system("leaks RPN");
 // }
 
-// int main(int ac, char **av) {
+int main(int argc, char **argv) {
 //   atexit(leaks);
-//   if (ac != 2) {
-//     std::cerr <<"Error" << std::endl;
-//     return 0;
-//   }
-//   RPN rpn(av[1]);
-//   return 0;
-// }
+    if (argc != 2) {
+        std::cerr <<"Error" << std::endl;
+        return 0;
+    }
+    RPN rpn(argv[1]);
+    rpn.ReversePolishNotation();
+    return 0;
+}
